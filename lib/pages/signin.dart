e04753b75b31ff7715821e7dabb058bccd0c1a52
Wayne_Scientist <vanderlinkinchat@gmail.com>
2025-07-09 15:53:47 +0000

    signup

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nyikarise/pages/signup.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -11,95 +13,99 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.close))
+        ]),
         body: Center(
-      child: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.bold,
-                fontSize: 26,
-                color: Color(0xFF1C1C1C),
-              ),
-            ),
-            SizedBox(height: 6),
-            Text(
-              'Sign In to continue',
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.normal,
-                fontSize: 18,
-                color: Color(0xFF1C1C1C),
-              ),
-            ),
-            SizedBox(height: 26),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 26),
-            SizedBox(
-              width: double.infinity,
-              height: 49,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF3B62FF),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: Text(
-                  'Login',
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Welcome',
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+                    fontFamily: 'Poppins',
                     fontWeight: FontWeight.bold,
+                    fontSize: 26,
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 26),
-            Center(
-              child: Text(
-                'Forgot Password?',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0xFF87879D),
+                const SizedBox(height: 6),
+                const Text(
+                  'To Nyikarise App',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Text(
-                "Don't have an account? Sign Up",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 14,
-                  color: Color(0xFF87879D),
+                const SizedBox(height: 26),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
+                const SizedBox(height: 16),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 26),
+                SizedBox(
+                  width: double.infinity,
+                  height: 49,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Get.theme.colorScheme.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 26),
+                const Center(
+                  child: Text(
+                    'Forgot Password?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF87879D),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Center(
+                  child: TextButton(
+                      onPressed: () {
+                        Get.to(const SignUp());
+                      },
+                      child: const Text(
+                        "Don't have an account? Sign Up",
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          color: Color(0xFF87879D),
+                        ),
+                      )),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
